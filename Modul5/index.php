58fa4525d7968befa5f433abe576ffd0e207da9b
Akmallullail Sya'ban <2310817310010@mhs.ulm.ac.id>
2025-05-10 10:31:32 +0800

    .

--- Modul5/index.php
+++ Modul5/index.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+    crossorigin="anonymous">
     <title>Menu Buku</title>
     <style>
         body {
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
-            background: url('https://png.pngtree.com/thumb_back/fw800/background/20241026/pngtree-a-peaceful-library-scene-with-an-open-book-on-table-perfect-image_16458585.jpg') no-repeat center center fixed;
+            background: url('perpus.jpg') 
+            no-repeat center center fixed;
             background-size: cover;
             display: flex;
             justify-content: center;
@@ -46,6 +50,5 @@
         <a href="Buku.php" class="btn btn-primary w-100">Buku</a>
         <a href="Peminjaman.php" class="btn btn-primary w-100">Peminjaman</a>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
