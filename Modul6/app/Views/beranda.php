fed575f4dc7bae2ff95982d4f12e1ca3d10f3ecd
Akmallullail Sya'ban <2310817310010@mhs.ulm.ac.id>
2025-05-13 19:16:34 +0800

    .

--- Modul6/app/Views/beranda.php
+++ Modul6/app/Views/beranda.php
@@ -2,6 +2,6 @@
 
 <?= $this->section('content') ?>
 <img src="<?= base_url('images/' . $biodata['gambar']) ?>" alt="Foto" class="img-thumbnail mb-3" width="200">
-<p>Nama: <?= $biodata['nama'] ?></p>
-<p>NIM: <?= $biodata['nim'] ?></p>
+<p><b>Nama: <?= $biodata['nama'] ?></b></p>
+<p><b>NIM: <?= $biodata['nim'] ?></b></p>
 <?= $this->endSection() ?>
